2326d44afcb79773be0e5716598328402e565162
Ralph Lange (CR/AEE1) <ralph.lange@de.bosch.com>
2020-03-12 12:37:11 +0100

    Renamed package to cbg-executor_ping-pong
    Signed-off-by: Ralph Lange (CR/AEE1) <ralph.lange@de.bosch.com>

--- rclcpp/cbg-executor_ping-pong/runBatchComputationLoad.sh
+++ rclcpp/cbg-executor_ping-pong/runBatchComputationLoad.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 rtSendPeriod=100000
 beSendPeriod=100000
@@ -6,7 +6,6 @@ rtComputeBusyLoop=25000
 beComputeBusyLoop=100000
 while [  $rtComputeBusyLoop -le  160000 ]; do
   echo "Rcv RT Compute Period is $rtComputeBusyLoop"
-  ros2 run minimal_multi-executors minimal_multi-executors sr $rtSendPeriod $beSendPeriod $rtComputeBusyLoop $beComputeBusyLoop >> resultsComputationLoad.txt
+  ros2 run cbg-executor_ping-pong_cpp ping-pong io $rtSendPeriod $beSendPeriod $rtComputeBusyLoop $beComputeBusyLoop >> resultsComputationLoad.txt
   let rtComputeBusyLoop=rtComputeBusyLoop+25000
 done
-
--- rclcpp/cbg-executor_ping-pong/runBatchIntraProcessCommunication.sh
+++ rclcpp/cbg-executor_ping-pong/runBatchIntraProcessCommunication.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 rtSendPeriod=4000
 beSendPeriod=1000
@@ -6,7 +6,6 @@ rtComputeBusyLoop=0
 beComputeBusyLoop=1000
 while [  $rtSendPeriod -ge 31 ]; do
   echo "rtSendPeriod in us is $rtSendPeriod"
-  ros2 run minimal_multi-executors minimal_multi-executors sr $rtSendPeriod $beSendPeriod $rtComputeBusyLoop $beComputeBusyLoop >> resultsIntraProcessCommunicationLoad.txt
+  ros2 run cbg-executor_ping-pong_cpp ping-pong io $rtSendPeriod $beSendPeriod $rtComputeBusyLoop $beComputeBusyLoop >> resultsIntraProcessCommunicationLoad.txt
   let rtSendPeriod=rtSendPeriod/2
 done
-
